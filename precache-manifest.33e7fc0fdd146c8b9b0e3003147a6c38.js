a0decf2dd37472974d4ea42deb6dc983597deb2c
DmytroTrutenko <55551711+DmytroTrutenko@users.noreply.github.com>
2020-05-20 22:16:27 +0300

    Updates

---
+++ precache-manifest.33e7fc0fdd146c8b9b0e3003147a6c38.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c08d50bc711110b461561ce94321131",
+    "url": "/educake/index.html"
+  },
+  {
+    "revision": "66763e048e2f94c60103",
+    "url": "/educake/static/css/2.b8d7bdcb.chunk.css"
+  },
+  {
+    "revision": "d8c250e7060bf3986f03",
+    "url": "/educake/static/css/main.86abe5dd.chunk.css"
+  },
+  {
+    "revision": "66763e048e2f94c60103",
+    "url": "/educake/static/js/2.3b608a9c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/educake/static/js/2.3b608a9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8c250e7060bf3986f03",
+    "url": "/educake/static/js/main.30afcaf6.chunk.js"
+  },
+  {
+    "revision": "b6595bc90ee982b24140",
+    "url": "/educake/static/js/runtime-main.6f86f252.js"
+  },
+  {
+    "revision": "2c42a9d93c1173b553871c9a3871498b",
+    "url": "/educake/static/media/IT_spec.2c42a9d9.jpg"
+  },
+  {
+    "revision": "555b16cbf87f6626014e4ddae59d285d",
+    "url": "/educake/static/media/bg_call_action2.555b16cb.jpg"
+  },
+  {
+    "revision": "494f7c52a182ff6298d95a2b1787bc4c",
+    "url": "/educake/static/media/coin.494f7c52.png"
+  },
+  {
+    "revision": "781920217cbf888149eb12d22c26a138",
+    "url": "/educake/static/media/doctors.78192021.jpg"
+  },
+  {
+    "revision": "4c2d92da2c7f39dc8de59222047cb909",
+    "url": "/educake/static/media/educakeLogo2.4c2d92da.png"
+  },
+  {
+    "revision": "88ce7cd0a584b8644d91b261c9707d2b",
+    "url": "/educake/static/media/eng.88ce7cd0.png"
+  },
+  {
+    "revision": "f8002fd36549e55b11c3bc25643ddde3",
+    "url": "/educake/static/media/fb.f8002fd3.png"
+  },
+  {
+    "revision": "20f96d43537478d493e4367f3751964a",
+    "url": "/educake/static/media/github.20f96d43.png"
+  },
+  {
+    "revision": "f4bb562923963662c3d25c075eec2801",
+    "url": "/educake/static/media/instagram.f4bb5629.png"
+  },
+  {
+    "revision": "16c0a46f62482d07fc3b90f817dbdd94",
+    "url": "/educake/static/media/kval_rab.16c0a46f.jpg"
+  },
+  {
+    "revision": "0790edfc14fed318b071baf2bc1acba5",
+    "url": "/educake/static/media/mainor.0790edfc.jpg"
+  },
+  {
+    "revision": "7df347d8aa1cb6a1a62d9e2d2719a627",
+    "url": "/educake/static/media/persons.7df347d8.png"
+  },
+  {
+    "revision": "077d4b726ba81764daae604de8a2b0c3",
+    "url": "/educake/static/media/site.077d4b72.png"
+  },
+  {
+    "revision": "9dbd1f688e454011191e3677213c7bcb",
+    "url": "/educake/static/media/tartuulikool.9dbd1f68.png"
+  },
+  {
+    "revision": "8edb137465a75714658bbf524839b80f",
+    "url": "/educake/static/media/team.8edb1374.jpg"
+  },
+  {
+    "revision": "a1cd3da63d535a71537ec16fee447a0e",
+    "url": "/educake/static/media/ttu.a1cd3da6.jpg"
+  },
+  {
+    "revision": "b757f2bca8039d4a08882d38f4c1b8f0",
+    "url": "/educake/static/media/twitter.b757f2bc.png"
+  }
+]);
