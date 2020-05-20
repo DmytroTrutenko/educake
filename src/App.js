2e725045741305be7354d63c5147aa277e70a870
DmytroTrutenko <55551711+DmytroTrutenko@users.noreply.github.com>
2020-05-20 22:26:01 +0300

    ++

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {BrowserRouter, Switch, Route, HashRouter} from "react-router-dom";
 import Search from "./components/Search";
 import About from "./components/About";
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Header/>
             <Switch>
                 <Route path={'/'} exact component={Main}/>
@@ -16,7 +16,7 @@ function App() {
                 <Route path={'/about'} component={About}/>
             </Switch>
             <Footer/>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
