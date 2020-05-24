4dbb8aafcbc84395aa0293140e29d4e5daaceb24
DmytroTrutenko <55551711+DmytroTrutenko@users.noreply.github.com>
2020-05-24 16:40:31 +0300

    +lang

---
+++ src/lang/en.js
@@ -0,0 +1,68 @@
+export default {
+    menu: {
+        main: 'Main',
+        search: 'Search',
+        about: 'About US',
+    },
+    main: {
+        call_action: {
+            h3_1: 'Looking for a university in Estonia where you could study?',
+            p1: 'Use our search to find the one that suits you best',
+            btn1: 'Search for university'
+        },
+        pop_univer: {
+            h4_1: 'Most popular universities among estonian matriculants',
+        },
+        pop_spec: {
+            h4_2: 'These 3 specialities were the most popular search criterias for our users',
+            li_1: 'IT-specialists',
+            l2_2: 'Economists',
+            li_3: 'Medical workers'
+        }
+    },
+    search: {
+        p2: 'University name:',
+        p3: 'Areas of study:',
+        p4: 'Grade:',
+        p5: 'Type of study programme:',
+        p6: 'Location:',
+        p7: 'Language:',
+        p8: 'Cost ',
+        p9: 'Duration of study',
+        p10: 'Bachelor, Master',
+        p11: 'Type: Full-time, Part-time',
+        p12: 'Language: eng, esst',
+        p13: 'Tallinn',
+        p14: 'Web-technology',
+        p15: 'This search engine was created for practical work at the university.',
+        span: 'Annual cost: 3860€',
+        btn4: 'Find out more',
+        select: {
+            option1: 'Bachelor',
+            option2: 'Master',
+            option3: 'Specialist',
+            option4: 'Full-time',
+            option5: 'Part-time',
+            option6: 'Session study',
+            option7: 'Таллинн',
+            option8: 'Тарту',
+            option9: 'Нарва'
+        },
+        label1: 'Russian',
+        label2: 'English',
+        label3: 'Estonian',
+        btn2: 'Search',
+        btn3: 'Clear',
+        h4_3: 'Search results',
+        h3_2: 'Tallinn University',
+
+    },
+    about: {
+        h4_4: 'Our team has created a platform for university searches for potential students in Estonia.',
+        h3_3: 'To create the site were used:',
+        li_4: 'Website Automation.',
+        li_5: 'Website budget development.',
+        li_6: 'Character development and user stories, as well as the script.',
+
+    }
+}
---
+++ src/lang/esst.js
@@ -0,0 +1,68 @@
+export default {
+    menu: {
+        main: 'Pealeht',
+        search: 'Otsing',
+        about: 'Meist',
+    },
+    main: {
+        call_action: {
+            h3_1: 'Otsid ülikooli Eestis?',
+            p1: 'Leia oma tulevikku õppeasutuse läbi otsingu',
+            btn1: 'Ülikooli otsing'
+        },
+        pop_univer: {
+            h4_1: 'Kõige populaarsemad õppeasutused eesti abiturientide seas',
+        },
+        pop_spec: {
+            h4_2: 'Allolevad erialad olid kõige otsivamad meie kasutajate seas',
+            li_1: 'IT-spetsialistid',
+            l2_2: 'Majandusteadlased',
+            li_3: 'Meditsiinitöötajad'
+        }
+    },
+    search: {
+        p2: 'Ülikooli nimi:',
+        p3: 'Eriala:',
+        p4: 'Grade:',
+        p5: 'Õppevorm:',
+        p6: 'Asukoht:',
+        p7: 'Keel:',
+        p8: 'Hind',
+        p9: 'Õppe kestus',
+        p10: 'Bakalaureus, Magister',
+        p11: 'Õppevorm: Päevaõpe, Kaugõpe',
+        p12: 'Keel: Inglise, Eestlane',
+        p13: 'Tallinn',
+        p14: 'Veebitehnoloogia',
+        p15: 'Данный поисковик был создан для практической работы в университете.',
+        span: 'Õppeaasta hind: 3860€',
+        btn4: 'Vaata lähemalt',
+        select: {
+            option1: 'Bakalaureus',
+            option2: 'Magister',
+            option3: 'Spetsialist',
+            option4: 'Päevaõpe',
+            option5: 'Kaugõpe',
+            option6: 'Sessiooniõpe',
+            option7: 'Tallinn',
+            option8: 'Tartu',
+            option9: 'Narva'
+        },
+        label1: 'Vene',
+        label2: 'Inglise',
+        label3: 'Eestlane',
+        btn2: 'Otsing',
+        btn3: 'Puhasta',
+        h4_3: 'Otsingutulemused',
+        h3_2: 'Tallinna Ülikool',
+
+    },
+    about:{
+        h4_4:'Meie meeskond on loonud platvormi ülikoolide otsimiseks potentsiaalsetele üliõpilastele Eestis.',
+        h3_3:'Saidi loomiseks kasutati:',
+        li_4: 'Veebisaidi automatiseerimine.',
+        li_5: 'Veebisaidi eelarve väljatöötamine.',
+        li_6: 'Tegelaskujude arendamine ja kasutajate lood, samuti skript.',
+
+    }
+}
